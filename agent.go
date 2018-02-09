316b085cc5f94bad67e42abac5ab5959d03ea8b7
Sebastien Boeuf <sebastien.boeuf@intel.com>
2018-02-08 22:21:56 -0800

    agent: Close console socket pair after it's been used
    This commit makes sure we don't leave a socket pair opened after it
    has been properly used to retrieve the terminal master end.
    
    Fixes #207
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- agent.go
+++ agent.go
@@ -952,6 +952,35 @@ func (c *container) closeProcessPipes(pid string) {
 	}
 }
 
+func (c *container) closeConsoleSocket(pid string) error {
+	cid := c.container.ID()
+	proc := c.getProcess(pid)
+
+	errMsg := "Could not close process socket pair"
+
+	if proc == nil {
+		return fmt.Errorf("Container %s process %s no longer exists", cid, pid)
+	}
+
+	if proc.consoleSock != nil {
+		if err := proc.consoleSock.Close(); err != nil {
+			return fmt.Errorf("%s: %s", errMsg, err)
+		}
+
+		proc.consoleSock = nil
+	}
+
+	if proc.process.ConsoleSocket != nil {
+		if err := proc.process.ConsoleSocket.Close(); err != nil {
+			return fmt.Errorf("%s: %s", errMsg, err)
+		}
+
+		proc.process.ConsoleSocket = nil
+	}
+
+	return nil
+}
+
 // Executed as a go routine to route stdout and stderr to the TTY channel.
 func (p *pod) routeOutput(seq uint64, stream *os.File, wg *sync.WaitGroup) {
 	fieldLogger := agentLog.WithField("sequence", seq)
@@ -990,6 +1019,7 @@ func (p *pod) runContainerProcess(cid, pid string, terminal bool, started chan e
 
 	defer func() {
 		ctr.closeProcessPipes(pid)
+		ctr.closeConsoleSocket(pid)
 		ctr.closeProcessStreams(pid)
 		ctr.deleteProcess(pid)
 		ctr.wgProcesses.Done()
@@ -1045,6 +1075,12 @@ func (p *pod) runContainerProcess(cid, pid string, terminal bool, started chan e
 	if terminal {
 		termMaster, err := utils.RecvFd(proc.consoleSock)
 		if err != nil {
+			return err
+		}
+
+		// No need to maintain this socket opened after we retrieved
+		// the master end of the terminal.
+		if err := ctr.closeConsoleSocket(pid); err != nil {
 			return err
 		}
 
